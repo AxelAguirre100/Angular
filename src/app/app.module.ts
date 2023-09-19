0876ffbc3bbec83d65e55f5b189a3aa94ddf205e
Axel Aguirre <axelentoo@gmail.com>
2023-09-19 13:39:30 -0300

    PrimeraEntrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { AsideComponent } from './aside/aside.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    AsideComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
